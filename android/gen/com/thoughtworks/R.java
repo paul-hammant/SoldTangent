42044d50d66e9b6c4581ae3005d46bb1512a9ee4
Dhruv Chandna <dhruvchandna@gmail.com>
2010-10-06 20:32:19 -0500

    Start of informational panel.

--- android/gen/com/thoughtworks/R.java
+++ android/gen/com/thoughtworks/R.java
@@ -14,11 +14,13 @@ public final class R {
         public static final int white=0x7f040000;
     }
     public static final class drawable {
-        public static final int icon=0x7f020000;
-        public static final int sold=0x7f020001;
-        public static final int tangent=0x7f020002;
+        public static final int ic_menu_info_details=0x7f020000;
+        public static final int icon=0x7f020001;
+        public static final int sold=0x7f020002;
+        public static final int tangent=0x7f020003;
     }
     public static final class id {
+        public static final int button=0x7f060001;
         public static final int cardImage=0x7f060000;
     }
     public static final class layout {
