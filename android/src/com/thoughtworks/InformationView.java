87fa511cbf8298db26a99f9c28a6de327fcbd49a
Dhruv Chandna <dhruvchandna@gmail.com>
2010-10-07 21:22:27 -0500

    Changed Information view to be a TextView.

--- android/src/com/thoughtworks/InformationView.java
+++ android/src/com/thoughtworks/InformationView.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.ImageView;
 
 public class InformationView extends Activity {
     @Override
@@ -12,9 +11,7 @@ public class InformationView extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.information);
         
-        ImageView homeScreenView = (ImageView)  findViewById(R.id.cardImage);
-        homeScreenView.setImageResource(R.drawable.sold);
-        homeScreenView.setOnClickListener(new OnClickListener() {
+        findViewById(R.id.informationView).setOnClickListener(new OnClickListener() {
 
             public void onClick(View imageView) {
                 finish();
